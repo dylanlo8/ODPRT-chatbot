5c3b3b37b52befb314e2a92dbf981afb4dfb91cc
shuyaoao <e0968244@u.nus.edu>
2025-02-08 16:26:07 +0800

    add semantic chunking script

---
+++ chatbot/backend/chunking/script.py
@@ -0,0 +1,23 @@
+from langchain_experimental.text_splitter import SemanticChunker
+from langchain_community.document_loaders import PyPDFLoader
+from langchain_community.embeddings.fastembed import FastEmbedEmbeddings
+import os
+
+
+os.environ["TOKENIZERS_PARALLELISM"] = "false"
+
+
+loader = PyPDFLoader("")  # path to documents
+documents = loader.load()
+embed_model = FastEmbedEmbeddings(model_name="BAAI/bge-base-en-v1.5")
+
+semantic_chunker = SemanticChunker(embed_model,
+                                   breakpoint_threshold_type="percentile")
+
+semantic_chunks = (semantic_chunker
+                   .create_documents([d.page_content for d in documents]))
+
+for semantic_chunk in semantic_chunks:
+    if "Effect of Pre-training Tasks" in semantic_chunk.page_content:
+        print(semantic_chunk.page_content)
+        print(len(semantic_chunk.page_content))
