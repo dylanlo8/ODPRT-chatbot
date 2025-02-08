bedf539cc73eaf1f78eff8b2e5aeb7ae459ab3ef
shuyaoao <e0968244@u.nus.edu>
2025-02-08 17:55:13 +0800

    add notebooks for web scraping and semantic chunking

--- chatbot/backend/chunking/script.py
+++ chatbot/backend/chunking/script.py
@@ -17,7 +17,7 @@ semantic_chunker = SemanticChunker(embed_model,
 semantic_chunks = (semantic_chunker
                    .create_documents([d.page_content for d in documents]))
 
-for semantic_chunk in semantic_chunks:
-    if "Effect of Pre-training Tasks" in semantic_chunk.page_content:
-        print(semantic_chunk.page_content)
-        print(len(semantic_chunk.page_content))
+# for semantic_chunk in semantic_chunks:
+#     if "Effect of Pre-training Tasks" in semantic_chunk.page_content:
+#         print(semantic_chunk.page_content)
+#         print(len(semantic_chunk.page_content))
