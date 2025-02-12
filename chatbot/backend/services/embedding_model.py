03c03f6a0cf8f261673a1ad18b684afb75aacaf3
shuyaoao <e0968244@u.nus.edu>
2025-02-12 12:18:23 +0800

    change embedding model

--- chatbot/backend/services/embedding_model.py
+++ chatbot/backend/services/embedding_model.py
@@ -1,26 +1,31 @@
 import torch
 from sentence_transformers import SentenceTransformer
 
+
 class EmbeddingModel:
+
     def __init__(self):
         self.embedding_model = SentenceTransformer('BAAI/bge-large-en-v1.5')
         self.embedding_model.eval()
-    
+
     def batch_encode(self, texts):
         """Generates dense embeddings for texts."""
         description_embeddings = []
 
         for text in texts:
-            description_embeddings.append(self.__encode_text(text))
-        
+            description_embeddings.append(self.embed_documents(text))
+
         return description_embeddings
-        
-    def __encode_text(self, text):
+
+    def embed_documents(self, text):
         """Generates dense embeddings for text."""
         with torch.no_grad():
-            # Generate embedding for summary of image           
-            description_embedding = self.embedding_model.encode(text, normalize_embeddings=True).tolist()
+            # Generate embedding for summary of image
+            description_embedding = (self.embedding_model
+                                     .encode(text, normalize_embeddings=True)
+                                     .tolist())
 
         return description_embedding
+
 
 embedding_model = EmbeddingModel()
