be7c3417102f2bebe16e8853590b9d80f6999a79
dylanlo8 <lojingjiedylan@gmail.com>
2025-02-18 14:43:57 +0800

    add bucketname

--- chatbot/backend/services/file_storage/buckets.py
+++ chatbot/backend/services/file_storage/buckets.py
@@ -8,10 +8,12 @@ url: str = os.environ.get("SUPABASE_URL")
 service_key: str = os.environ.get("SUPABASE_SERVICE_KEY") # Requires service role key to bypass RLS
 supabase: Client = create_client(url, supabase_key=service_key)
 
+BUCKET_NAME = "rag_files"
+
 # Some examples of MIME File Types
 # text/plain, image/png, image/jpeg, image/jpg, application/pdf
 
-def upload_file(source_file_path: str, bucket_name: str, destination_file_path: str) -> dict:
+def upload_file(source_file_path: str, destination_file_path: str) -> dict:
     """
     Uploads a file to the bucket
     """
@@ -23,7 +25,7 @@ def upload_file(source_file_path: str, bucket_name: str, destination_file_path: 
 
         # Open the file in binary mode for uploading
         with open(source_file_path, 'rb') as file:
-            response = supabase.storage.from_(bucket_name).upload(
+            response = supabase.storage.from_(BUCKET_NAME).upload(
                 destination_file_path, 
                 file,
                 {"content-type": content_type}
@@ -32,12 +34,12 @@ def upload_file(source_file_path: str, bucket_name: str, destination_file_path: 
     except Exception as exception:
         return exception
     
-def delete_file(bucket_name: str, file_names: list[str]) -> dict:
+def delete_file(file_names: list[str]) -> dict:
     """
     Bulk deletes files from the bucket
     """
     try:
-        response = supabase.storage.from_(bucket_name).remove(file_names)
+        response = supabase.storage.from_(BUCKET_NAME).remove(file_names)
         return response
     except Exception as exception:
         return exception
