0e12ebd6c371f7e81a5f9858f841f2bf8ae87ea9
Cherie <cherrypighippo532@gmail.com>
2025-03-02 17:07:58 +0800

    added dashboard ui

---
+++ admin/frontend/src/components/bar/BarChart.jsx
@@ -0,0 +1,122 @@
+import { ResponsiveBar } from "@nivo/bar";
+import { tokens } from "../../theme";
+
+const BarChart = ({ data, keys, index, showLegend}) => {
+  const colors = tokens();
+
+  return (
+    <ResponsiveBar
+      data={data}
+      theme={{
+
+        axis: {
+          domain: {
+            line: {
+              stroke: "transparent",
+            },
+          },
+          legend: {
+            text: {
+              fill: colors.text,
+            },
+          },
+          ticks: {
+            line: {
+              stroke: colors.text,
+              strokeWidth: 1,
+            },
+            text: {
+              fill: colors.text,
+            },
+          },
+        },
+        legends: {
+          text: {
+            fill: colors.comp_background,
+          },
+        },
+      }}
+      keys={ keys }
+      indexBy={ index }
+      colors={({ id, data }) => data[`${id}Color`]}
+      margin={{ top: 30, right: 60 , bottom: showLegend? 60:20, left: 60 }}
+      padding={0.5}
+      valueScale={{ type: "linear" }}
+      indexScale={{ type: "band", round: true }}
+      defs={[
+        {
+          id: "dots",
+          type: "patternDots",
+          background: "inherit",
+          color: "#38bcb2",
+          size: 4,
+          padding: 1,
+          stagger: true,
+        },
+        {
+          id: "lines",
+          type: "patternLines",
+          background: "inherit",
+          color: "#eed312",
+          rotation: -45,
+          lineWidth: 6,
+          spacing: 10,
+        },
+      ]}
+      borderColor={{
+        from: "color",
+        modifiers: [["darker", "1.6"]],
+      }}
+      axisTop={null}
+      axisRight={null}
+      axisBottom={{
+        tickSize: 0,
+        tickPadding: 5,
+        tickRotation: 0,
+        legendOffset: 32,
+      }}
+      axisLeft={{
+        tickSize: 0,
+        tickPadding: 5,
+        tickRotation: 0,
+        legendOffset: -40,
+      }}
+      enableLabel={false}
+      enableGridY={false}
+      labelSkipWidth={12}
+      labelSkipHeight={12}
+      labelTextColor={{
+        from: "color",
+        modifiers: [["darker", 1.6]],
+      }}
+      legends={
+        showLegend
+        ? [
+        {
+          dataFrom: "keys",
+          anchor: "bottom",
+          direction: "row",
+          justify: false,
+          translateX: 0,
+          translateY: 50,
+          itemsSpacing: 0,
+          itemWidth: 100,
+          itemHeight: 0,
+          itemTextColor: colors.text,
+          itemDirection: "left-to-right",
+          itemOpacity: 1,
+          symbolSize: 15,
+          symbolShape: "circle",
+        },
+      ] 
+      : []
+    }
+      role="application"
+      barAriaLabel={function (e) {
+        return e.id + ": " + e.formattedValue + " in country: " + e.indexValue;
+      }}
+    />
+  );
+};
+
+export default BarChart;
---
+++ admin/frontend/src/components/pie/PieChart.jsx
@@ -0,0 +1,98 @@
+import { ResponsivePie } from "@nivo/pie";
+import { tokens } from "../../theme";
+
+const PieChart = ({ data }) => {
+  const colors = tokens();
+  const total = data.reduce((sum, item) => sum + item.value, 0);
+  
+  return (
+    <ResponsivePie
+      data={data}
+      theme={{
+        axis: {
+          domain: {
+            line: {
+              stroke: colors.text,
+            },
+          },
+          legend: {
+            text: {
+              fill: colors.text,
+            },
+          },
+          ticks: {
+            line: {
+              stroke: colors.text,
+              strokeWidth: 1,
+            },
+            text: {
+              fill: colors.text,
+            },
+          },
+        },
+        legends: {
+          text: {
+            fill: colors.text,
+          },
+        },
+      }}
+      margin={{right: 80, bottom: 50, left: 80 }}
+      innerRadius={0.5}
+      padAngle={0.7}
+      cornerRadius={3}
+      activeOuterRadiusOffset={8}
+      borderColor={{
+        from: "color",
+        modifiers: [["darker", 0.2]],
+      }}
+      enableArcLabels={true}
+      enableArcLinkLabels={false}
+      arcLabel={(e) => `${((e.value / total) * 100).toFixed(1)}%`} 
+      arcLabelsRadiusOffset={0.5} 
+      arcLabelsSkipAngle={10} 
+      arcLabelsTextColor={({ data }) =>
+        data.color === tokens().gray[500] ? "#000000" : "#FFFFFF"
+      }
+      colors={({ data }) => data.color} 
+      defs={[
+        {
+          id: "dots",
+          type: "patternDots",
+          background: "inherit",
+          color: "rgba(255, 255, 255, 0.3)",
+          size: 4,
+          padding: 1,
+          stagger: true,
+        },
+        {
+          id: "lines",
+          type: "patternLines",
+          background: "inherit",
+          color: "rgba(255, 255, 255, 0.3)",
+          rotation: -45,
+          lineWidth: 6,
+          spacing: 10,
+        },
+      ]}
+      legends={[
+        {
+          anchor: "bottom",
+          direction: "row",
+          justify: false,
+          translateX: 0,
+          translateY: 45,
+          itemsSpacing: 0,
+          itemWidth: 100,
+          itemHeight: 20,
+          itemTextColor: colors.text,
+          itemDirection: "left-to-right",
+          itemOpacity: 1,
+          symbolSize: 15,
+          symbolShape: "circle",
+        },
+      ]}
+    />
+  );
+};
+
+export default PieChart;
