f3b1401d6650fc0afb3a111c11217914e22e73c3
dylanlo8 <lojingjiedylan@gmail.com>
2025-03-03 15:42:14 +0800

    cleanup: variable names text_ingestion

--- chatbot/backend/ingestion/text_ingestion.py
+++ chatbot/backend/ingestion/text_ingestion.py
@@ -3,18 +3,21 @@ import pickle
 import requests
 from chatbot.backend.services.models.embedding_model import embedding_model  # Ensure this import is present
 
+# Ensure that API Service is up and running
+# Set directory where all the data files are located
+data_dir = "chatbot/backend/data_pkl"
+
 def ingest_text_documents():
-    # Define the directory containing the pickle files
-    pkl_dir = "chatbot/backend/data_pkl"
-    
-    # Iterate through all subdirectories in the pkl_dir
-    for dir in os.listdir(pkl_dir):
-        sub_dir_path = os.path.join(pkl_dir, dir)
+    # Iterate through all subdirectories in the data_dir
+    for dir in os.listdir(data_dir):
+        sub_dir_path = os.path.join(data_dir, dir)
         if os.path.isdir(sub_dir_path):  # Check if it's a directory
             # Walk through each subdirectory to find .pkl files
             for root, _, files in os.walk(sub_dir_path):
                 for filename in files:
                     file_path = os.path.join(root, filename)
+
+                    # 1: Open Pickle Files to access QA-Pairs
                     if filename.endswith(".pkl"):
                         # Load the text chunks from the pickle file
                         with open(file_path, "rb") as f:
@@ -43,7 +46,7 @@ def ingest_text_documents():
                         except Exception as e:
                             print(f"Failed to ingest data from {filename}: {str(e)}")
 
-                    # Upload the mail original documents into the FileStorage
+                    # 2: Upload the mail original documents into the FileStorage
                     if filename.endswith(".msg"):
                         try:
                             with open(file_path, "rb") as f:
