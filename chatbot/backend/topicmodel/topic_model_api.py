10d2d7bd3c68c6cb805f6275395311880b9fe378
aldensiol <aldensio@live.com>
2025-03-09 01:06:16 +0800

    feat: add router to main api.py

--- chatbot/backend/topicmodel/topic_model_api.py
+++ chatbot/backend/topicmodel/topic_model_api.py
@@ -21,17 +21,9 @@ def map_topics(request: TopicModelRequest):
     try:
         # map the topics
         topics = simple_tm.map_all_topics(qa_pairs=request.qa_pairs)
-        # TODO: save the topics AND qa_pairs to supabase
-        resp = bulk_insert_conversations(
-            [
-                {"qa_pair": qa_pair, "topic": topic}
-                for qa_pair, topic in zip(request.qa_pairs, topics)
-            ]
-        )
         return {
             "status": "success",
             "topics": topics,
-            "message": resp,
         }
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
