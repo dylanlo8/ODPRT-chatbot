6adca7709bd448e97c0b3e5049e43944b477976e
aldensiol <aldensio@live.com>
2025-03-28 00:04:48 +0800

    docs: adjusting readme

--- deprecated/backend/main_OLDGROUP.py
+++
@@ -1,9 +0,0 @@
-from database_OLDGROUP.chroma_db import get_global_collection
-from chatbot.backend._OLDGROUP.server_OLDGROUP import create_server
-
-# Creates backend server and initialises vector database
-app = create_server()
-collection = get_global_collection()
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8000, debug=True)
